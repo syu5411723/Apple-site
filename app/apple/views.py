145d0fd6d4cb29093909246937223d5408f7c892
syuki <syu54@icloud.com>
2020-11-10 15:13:26 +0900

    check

--- app/apple/views.py
+++ app/apple/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
 from .models import Device
 
 
 class Index(TemplateView):
-    template_name = 'index.html'
+    template_name = 'detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        device_list = Device.objects.all()
+        context = {
+            'device_list': device_list
+        }
+        return context
 
 
 class DeviceDetail(DetailView):
