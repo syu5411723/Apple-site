63b205e5304563c0eea7ae4876ea460672311c19
syuki <syu54@icloud.com>
2020-11-10 22:04:29 +0900

    check commit

--- app/apple/urls.py
+++ app/apple/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('choice', views.Choice.as_view(), name='choice'),
-    path('detail', views.DeviceDetail.as_view(), name='detail'),
+    path('device_choice', views.DeviceChoice.as_view(), name='device_choice'),
+    path('device_table', views.DeviceTable.as_view(), name='device_table'),
+    path('test', views.Test.as_view(), name='test'),
 ]
--- app/apple/views.py
+++ app/apple/views.py
@@ -7,28 +7,26 @@ class Index(TemplateView):
     template_name = 'index.html'
 
 
-class DeviceDetail(TemplateView):
+class DeviceTable(TemplateView):
     model = Device
-    template_name = 'detail.html'
+    template_name = 'device_choice.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        device_list = Device.objects.all()
-        context = {
-            'device_list': device_list
-        }
-        return context
+    def get_queryset(self):
+        query = Device.objects.all()
+        if 'query' in self.request.GET:
+            qs = self.request.GET['query']
+            query = query.filter(name__exact=qs)
+
+        return query
 
 
-class Choice(TemplateView):
+class DeviceChoice(ListView):
     model = Device
-    template_name = 'choice.html'
+    template_name = 'device_choice.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        device_list = Device.objects.all()
-        context = {
-            'device_list': device_list
-        }
-        return context
+
+class Test(ListView):
+    model = Device
+    template_name = 'test.html'
+
 
