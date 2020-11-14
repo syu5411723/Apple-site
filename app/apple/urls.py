ea92b71f4da846573a757e34a43dbc60b7040290
syuki <syu54@icloud.com>
2020-11-14 15:31:10 +0900

    first complete

--- app/apple/urls.py
+++ app/apple/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('device_list', views.DeviceSearch, name='device_list'),
+
 ]
--- app/apple/views.py
+++ app/apple/views.py
@@ -11,16 +11,19 @@ class Index(TemplateView):
 def DeviceSearch(request):
     print("[request method]", request.method)
     if request.method == "POST":
-        name1 = request.POST.get('phone_name_1')
-        name2 = request.POST.get('phone_name_2')
-        device_model_a = DeviceModel.objects.filter(name__exact=name1)
-        device_model_b = DeviceModel.objects.filter(name__exact=name2)
+        name1 = request.POST.get('name1')
+        name2 = request.POST.get('name2')
+        device_model_a = DeviceModel.objects.filter(name__exact=name1).first()
+        device_model_b = DeviceModel.objects.filter(name__exact=name2).first()
         context = {
-            'device_models': (device_model_a, device_model_b)
+            'device_models': [device_model_a, device_model_b]
         }
+        return render(request, 'device_table.html', context)
     else:
         context = {'device_models': DeviceModel.objects.all()}
-    return render(request, 'device_list.html', context)
+        return render(request, 'device_list.html', context)
+
+
 
 
 
